cb7359e8a4b5796b84364eefa2b0bb15965d9d44
jonataspinto <jonatastdb_jp@hotmail.com>
2023-06-21 01:41:55 -0300

    feat(components/header): adds go back button and logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
+import { Logo } from '@/components/icons/Logo';
+import { ChevronLeft } from '@/components/icons/ChevronLeft';
+import { useLang } from '@/contexts/langContext';
+import styles from './header.module.scss';
+
 export const Header = () => {
-  return <header>Header component</header>;
+  const { lang } = useLang();
+
+  return (
+    <header className={styles.container}>
+      <button
+        className={styles.goBackButton}
+        aria-label={lang.goToBackButtonLabel}
+      >
+        <ChevronLeft />
+      </button>
+      <Logo ariaLabel={lang.logoAltText} />
+    </header>
+  );
 };
