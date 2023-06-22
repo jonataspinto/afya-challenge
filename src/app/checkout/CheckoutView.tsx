6a92febcd6e9f83d6e8720efa55e0326582bdd5f
jonataspinto <jonatastdb_jp@hotmail.com>
2023-06-22 14:21:36 -0300

    feat(app/checkout): adds `AvailablePlans`components in the view

--- src/app/checkout/CheckoutView.tsx
+++ src/app/checkout/CheckoutView.tsx
@@ -4,6 +4,8 @@ import { PaymentForm } from '@/components/PaymentForm';
 import { EmailTag } from '@/components/EmailTag';
 import { Tooltip } from '@/components/Tooltip';
 import { CreditCards } from '@/components/CreditCards';
+import { AvailablePlans } from '@/components/AvailablePlans';
+import { availablePlansMock } from '@/mock/availablePlans';
 
 import styles from './page.module.scss';
 
@@ -28,17 +30,17 @@ export const CheckoutView = () => {
       </section>
 
       <section className={styles.plansSection}>
-        <h2 className={styles.plansSection_heading}>
-          {checkoutPage.plansSection.heading}
-        </h2>
-        <EmailTag
-          email="fulano@cicrano.com.br"
-          className={styles.plansSection_tag}
-        />
-        <Tooltip
-          className={styles.plansSection_info}
-          label={checkoutPage.plansSection.info}
-        />
+        <div>
+          <h2 className={styles.plansSection_heading}>
+            {checkoutPage.plansSection.heading}
+          </h2>
+          <EmailTag
+            email="fulano@cicrano.com.br"
+            className={styles.plansSection_tag}
+          />
+        </div>
+        <AvailablePlans plans={availablePlansMock} />
+        <Tooltip label={checkoutPage.plansSection.info} />
       </section>
     </>
   );
