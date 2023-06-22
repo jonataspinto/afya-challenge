fe7fc2abcbb4aad8a7c9f5cc41f66f2b1138a367
jonataspinto <jonatastdb_jp@hotmail.com>
2023-06-21 22:18:51 -0300

    feat(app/checkout): adds elements in view based on test

--- src/app/checkout/CheckoutView.tsx
+++ src/app/checkout/CheckoutView.tsx
@@ -1,6 +1,9 @@
 'use client';
-import { PaymentForm } from '@/components/PaymentForm';
 import { useLang } from '@/contexts/langContext';
+import { PaymentForm } from '@/components/PaymentForm';
+import { EmailTag } from '@/components/EmailTag';
+import { Tooltip } from '@/components/Tooltip';
+import styles from './page.module.scss';
 
 export const CheckoutView = () => {
   const {
@@ -9,14 +12,28 @@ export const CheckoutView = () => {
 
   return (
     <>
-      <section>
-        <h2>{checkoutPage.paymentDataSection.heading}</h2>
-        <p>{checkoutPage.paymentDataSection.description}</p>
-        <PaymentForm onSubmit={() => console.log('pamentForm')} />
+      <section className={styles.paymentDataSection}>
+        <h2 className={styles.paymentDataSection_heading}>
+          {checkoutPage.paymentDataSection.heading}
+        </h2>
+        <p className={styles.paymentDataSection_description}>
+          {checkoutPage.paymentDataSection.description}
+        </p>
+        <PaymentForm onSubmit={() => {}} />
       </section>
-      <section>
-        <h2>{checkoutPage.plansSection.heading}</h2>
-        <span>{checkoutPage.plansSection.info}</span>
+
+      <section className={styles.plansSection}>
+        <h2 className={styles.plansSection_heading}>
+          {checkoutPage.plansSection.heading}
+        </h2>
+        <EmailTag
+          email="fulano@cicrano.com.br"
+          className={styles.plansSection_tag}
+        />
+        <Tooltip
+          className={styles.plansSection_info}
+          label={checkoutPage.plansSection.info}
+        />
       </section>
     </>
   );
