ebb1a09f86078e5fead499548a1c3ed273101a03
jonataspinto <jonatastdb_jp@hotmail.com>
2023-06-23 11:32:25 -0300

    test(components/purchaseData):  implement tests

---
+++ src/components/PurchaseData/purchaseData.spec.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import { LangProvider } from '@/contexts/langContext';
+import { purchaseDataMock } from '@/mock/purchase';
+import { availablePlansMock } from '@/mock/availablePlans';
+import { userDataMock } from '@/mock/userData';
+import { formatCpf, formatPlanTitle } from '@/utils/formatters';
+import { PurchaseData } from './';
+
+const contractedPlan = availablePlansMock[0];
+
+describe('PurchaseData', () => {
+  it('should render data of purchase', () => {
+    const contractedPlanDescQuery = 'contracted plan';
+
+    render(
+      <LangProvider>
+        <PurchaseData purchase={purchaseDataMock} userData={userDataMock} />
+      </LangProvider>,
+    );
+
+    expect(screen.getByRole('img', { name: /star icon/i })).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', {
+        name: new RegExp(formatPlanTitle(contractedPlan), 'i'),
+      }),
+    ).toBeInTheDocument();
+
+    expect(screen.getByTestId(contractedPlanDescQuery)).toBeInTheDocument();
+
+    expect(screen.getByText(userDataMock.email)).toBeInTheDocument();
+
+    expect(screen.getByText(formatCpf(userDataMock.cpf))).toBeInTheDocument();
+  });
+});
