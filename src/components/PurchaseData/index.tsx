0cefb116223b3b94a857b359b9008687010a2775
jonataspinto <jonatastdb_jp@hotmail.com>
2023-06-23 11:32:49 -0300

    feat(components/purchaseData):  implement component

---
+++ src/components/PurchaseData/index.tsx
@@ -0,0 +1,94 @@
+import { HTMLAttributes } from 'react';
+import { availablePlansMock } from '@/mock/availablePlans';
+import {
+  formatPlanInstallment,
+  formatPlanPrice,
+  formatPlanTitle,
+  formatCpf,
+} from '@/utils/formatters';
+import { Star } from '../icons/Star';
+import styles from './purchaseData.module.scss';
+
+interface PurchaseDataProps extends HTMLAttributes<HTMLDivElement> {
+  purchase: {
+    couponCode?: string;
+    creditCardCPF: string;
+    creditCardCVV: string;
+    creditCardExpirationDate: string;
+    creditCardHolder: string;
+    creditCardNumber: string;
+    gateway: string;
+    installments: number;
+    offerId: number;
+    userId: number;
+    id: number;
+  };
+  userData: {
+    email: string;
+    cpf: string;
+  };
+}
+
+export const PurchaseData = ({
+  purchase,
+  userData,
+  className,
+  ...restProps
+}: PurchaseDataProps) => {
+  const contractedPlan = availablePlansMock.find(
+    ({ id }) => id === purchase.offerId,
+  );
+
+  if (!contractedPlan) {
+    return null;
+  }
+
+  return (
+    <div
+      {...restProps}
+      className={`${styles.container} ${className ? className : ''}`}
+    >
+      <div className={styles.contractedPlan}>
+        <span className={styles.contractedPlan_starIcon}>
+          <Star ariaLabel="star icon" />
+        </span>
+        <div>
+          <h3 className={styles.contractedPlan_title}>
+            {formatPlanTitle(contractedPlan)}
+          </h3>
+          <span
+            className={styles.contractedPlan_condition}
+            data-testid="contracted plan"
+          >
+            {`${formatPlanPrice(
+              contractedPlan.fullPrice,
+            )} | ${formatPlanInstallment(contractedPlan, 'clean')}`}
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.userData}>
+        <span
+          className={`${styles.userData_label} ${styles.userData_emailLabel}`}
+        >
+          E-mail
+        </span>
+        <span
+          className={`${styles.userData_label} ${styles.userData_cpfLabel}`}
+        >
+          CPF
+        </span>
+        <span
+          className={`${styles.userData_value} ${styles.userData_emailValue}`}
+        >
+          {userData?.email}
+        </span>
+        <span
+          className={`${styles.userData_value} ${styles.userData_cpfValue}`}
+        >
+          {formatCpf(userData?.cpf)}
+        </span>
+      </div>
+    </div>
+  );
+};
