5dcf4a398de9cb9c974ea4e98c835acd62f969e6
jonataspinto <jonatastdb_jp@hotmail.com>
2023-06-26 18:55:20 -0300

    feat(components/header): adding validation to render goback button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,11 @@ import { ChevronLeft } from '@/components/icons/ChevronLeft';
 import { useLang } from '@/contexts/langContext';
 import styles from './header.module.scss';
 
-export const Header = () => {
+type HeaderProps = {
+  showGoBackButton?: boolean;
+};
+
+export const Header = ({ showGoBackButton = true }: HeaderProps) => {
   const { lang } = useLang();
   const router = useRouter();
   const handleClick = useCallback(() => {
@@ -15,13 +19,15 @@ export const Header = () => {
 
   return (
     <header className={styles.container}>
-      <button
-        className={styles.goBackButton}
-        aria-label={lang.goToBackButtonLabel}
-        onClick={handleClick}
-      >
-        <ChevronLeft ariaLabel={`${lang.goToBackButtonLabel}: icon`} />
-      </button>
+      {showGoBackButton && (
+        <button
+          className={styles.goBackButton}
+          aria-label={lang.goToBackButtonLabel}
+          onClick={handleClick}
+        >
+          <ChevronLeft ariaLabel={`${lang.goToBackButtonLabel}: icon`} />
+        </button>
+      )}
       <Logo ariaLabel={lang.logoAltText} />
     </header>
   );
