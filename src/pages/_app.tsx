28d132458091a6e0686655a8b6646958ba15059a
jonataspinto <jonatastdb_jp@hotmail.com>
2023-06-26 15:52:03 -0300

    feat(pages/_app): adding Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import NextNProgress from 'nextjs-progressbar';
 import { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 import { LangProvider } from '@/contexts/langContext';
+import { Header } from '@/components/Header';
+
 import '@/styles/index.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -9,6 +11,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <LangProvider>
       <Toaster />
       <NextNProgress color="#f5850b" height={4} />
+      <Header />
       <Component {...pageProps} />;
     </LangProvider>
   );
