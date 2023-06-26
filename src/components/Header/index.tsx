70a3429c455bcaf20b4dbccca34ca71157cf3ab8
jonataspinto <jonatastdb_jp@hotmail.com>
2023-06-26 16:23:22 -0300

    feat(components/header): implements goBack method and fixing styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { useCallback } from 'react';
+import { useRouter } from 'next/router';
 import { Logo } from '@/components/icons/Logo';
 import { ChevronLeft } from '@/components/icons/ChevronLeft';
 import { useLang } from '@/contexts/langContext';
@@ -6,12 +8,17 @@ import styles from './header.module.scss';
 
 export const Header = () => {
   const { lang } = useLang();
+  const router = useRouter();
+  const handleClick = useCallback(() => {
+    router.back();
+  }, [router]);
 
   return (
     <header className={styles.container}>
       <button
         className={styles.goBackButton}
         aria-label={lang.goToBackButtonLabel}
+        onClick={handleClick}
       >
         <ChevronLeft ariaLabel={`${lang.goToBackButtonLabel}: icon`} />
       </button>
