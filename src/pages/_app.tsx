c4aa272bacd371f07c9ccc148d8d9cbd2d34ac4f
jonataspinto <jonatastdb_jp@hotmail.com>
2023-06-26 15:32:11 -0300

    feat(pages/_app): implements `Toaster` and `ProgressBar`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
+import NextNProgress from 'nextjs-progressbar';
 import { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
 import { LangProvider } from '@/contexts/langContext';
 import '@/styles/index.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LangProvider>
+      <Toaster />
+      <NextNProgress color="#191847" />
       <Component {...pageProps} />;
     </LangProvider>
   );
