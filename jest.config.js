38a397a65feb6f44f683180b3cae827492b9d576
jonataspinto <jonatastdb_jp@hotmail.com>
2023-06-27 01:36:27 -0300

    test(jest.config): configure coverage ignore pattern

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  setupFilesAfterEnv: ["./jest.setup.js"],
-  testEnvironment: "jsdom",
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
   moduleNameMapper: {
-    "\\.(css|scss|less|jpg|png|svg)$": "identity-obj-proxy",
-    "@/(.*)": "<rootDir>/src/$1",
+    '\\.(css|scss|less|jpg|png|svg)$': 'identity-obj-proxy',
+    '@/(.*)': '<rootDir>/src/$1',
   },
+  coveragePathIgnorePatterns: ['<rootDir>/src/api/*'],
 };
