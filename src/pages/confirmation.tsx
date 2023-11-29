85e325d8f5fb10daef11e25b2ab1a47d0fd85254
jonataspinto <jonatastdb_jp@hotmail.com>
2023-11-29 20:04:54 -0300

    feat(pages/confirmation): implements prefetch of purchase

--- src/pages/confirmation.tsx
+++ src/pages/confirmation.tsx
@@ -1,14 +1,28 @@
 import { GetServerSideProps } from 'next';
 import { getPurchaseData } from '@/api/purchase';
+import { bootStrapReactQuery } from '@/api/clients/bootStrapReactQuery';
+import { QueriesKey } from '@/api/constants/queries';
 
 export { ConfirmationView as default } from '@/views/ConfirmationView';
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const purchase = await getPurchaseData();
+  const { queryClient, addQueriesToPrefetch, getDehydratedState } =
+    bootStrapReactQuery();
+
+  const prefetchPurchaseData = () => {
+    return queryClient.prefetchQuery({
+      queryKey: [QueriesKey.purchase],
+      queryFn: getPurchaseData,
+    });
+  };
+
+  addQueriesToPrefetch(prefetchPurchaseData);
+
+  const dehydratedState = await getDehydratedState();
 
   return {
     props: {
-      purchase,
+      dehydratedState,
     },
   };
 };
