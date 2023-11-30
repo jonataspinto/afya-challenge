f2df9ccfa97c8f7b74af1640594bc94692b661fe
jonataspinto <jonatastdb_jp@hotmail.com>
2023-11-30 00:38:00 -0300

    refactor(pages/confirmation): receive required param id

--- src/pages/confirmation/[id].tsx
+++ src/pages/confirmation/[id].tsx
@@ -5,18 +5,21 @@ import { QueriesKey } from '@/api/constants/queries';
 
 export { ConfirmationView as default } from '@/views/ConfirmationView';
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const id = params?.id;
   const { queryClient, addQueriesToPrefetch, getDehydratedState } =
     bootStrapReactQuery();
 
-  const prefetchPurchaseData = () => {
-    return queryClient.prefetchQuery({
-      queryKey: [QueriesKey.purchase],
-      queryFn: getPurchaseData,
-    });
-  };
+  if (id) {
+    const prefetchPurchaseData = () => {
+      return queryClient.prefetchQuery({
+        queryKey: [QueriesKey.purchase],
+        queryFn: () => getPurchaseData(id as string),
+      });
+    };
 
-  addQueriesToPrefetch(prefetchPurchaseData);
+    addQueriesToPrefetch(prefetchPurchaseData);
+  }
 
   const dehydratedState = await getDehydratedState();
 
