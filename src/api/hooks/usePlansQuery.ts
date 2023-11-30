e46b278670b08b94654050222f743b13cad4f4e8
jonataspinto <jonatastdb_jp@hotmail.com>
2023-11-30 11:01:47 -0300

    feat(api/hooks): adding queryOptions param in queries
    - usePlansQuery
    - usePurchaseQuery

--- src/api/hooks/usePlansQuery.ts
+++ src/api/hooks/usePlansQuery.ts
@@ -1,7 +1,21 @@
-import { useQuery } from '@tanstack/react-query';
+import { QueryKey, UseQueryOptions, useQuery } from '@tanstack/react-query';
 import { getPlans } from '../getPlans';
 import { QueriesKey } from '../constants/queries';
 
-export const usePlansQuery = () => {
-  return useQuery({ queryKey: [QueriesKey.plans], queryFn: getPlans });
+export const usePlansQuery = (
+  queryOptions?: Omit<
+    UseQueryOptions<
+      PlanDTO[] | undefined,
+      unknown,
+      PlanDTO[] | undefined,
+      QueryKey
+    >,
+    'queryFn'
+  >,
+) => {
+  return useQuery({
+    queryKey: [QueriesKey.plans],
+    queryFn: getPlans,
+    ...queryOptions,
+  });
 };
