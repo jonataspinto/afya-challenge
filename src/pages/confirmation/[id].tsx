cb4783508055ef438e6093356f353d2f65658cc5
jonataspinto <jonatastdb_jp@hotmail.com>
2023-11-30 09:28:09 -0300

    feat(pages/confirmation): adding `getPurchaseDataServer` function

--- src/pages/confirmation/[id].tsx
+++ src/pages/confirmation/[id].tsx
@@ -1,11 +1,12 @@
 import { GetServerSideProps } from 'next';
-import { getPurchaseData } from '@/api/purchase';
+import { getPurchaseDataServer } from '@/api/purchase';
 import { bootStrapReactQuery } from '@/api/clients/bootStrapReactQuery';
 import { QueriesKey } from '@/api/constants/queries';
 
 export { ConfirmationView as default } from '@/views/ConfirmationView';
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps = async (pageContext) => {
+  const { params } = pageContext;
   const id = params?.id;
   const { queryClient, addQueriesToPrefetch, getDehydratedState } =
     bootStrapReactQuery();
@@ -14,7 +15,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     const prefetchPurchaseData = () => {
       return queryClient.prefetchQuery({
         queryKey: [QueriesKey.purchase],
-        queryFn: () => getPurchaseData(id as string),
+        queryFn: () => getPurchaseDataServer({ id, pageContext }),
       });
     };
 
