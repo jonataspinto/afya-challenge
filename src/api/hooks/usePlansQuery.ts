e4aff125ff62532e9206a78b6670667b8b01ed10
jonataspinto <jonatastdb_jp@hotmail.com>
2023-11-30 09:23:39 -0300

    refactor(api/hooks/usPlansQuery): changing name of function

--- src/api/hooks/usePlansQuery.ts
+++ src/api/hooks/usePlansQuery.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
-import { getPLans } from '../getPlans';
+import { getPlans } from '../getPlans';
 import { QueriesKey } from '../constants/queries';
 
 export const usePlansQuery = () => {
-  return useQuery({ queryKey: [QueriesKey.plans], queryFn: getPLans });
+  return useQuery({ queryKey: [QueriesKey.plans], queryFn: getPlans });
 };
