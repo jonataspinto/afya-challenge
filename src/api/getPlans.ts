64a9ff0718099f5ee89d6caf4833ba2fb0d16e6b
jonataspinto <jonatastdb_jp@hotmail.com>
2023-11-30 09:19:24 -0300

    feat(api/getPlans): implements function to server

--- src/api/getPlans.ts
+++ src/api/getPlans.ts
@@ -1,6 +1,20 @@
+import { ParsedUrlQuery } from 'querystring';
 import { HttpClient } from './clients/httpClient';
+import { GetServerSidePropsContext, PreviewData } from 'next';
 
-export const getPLans = async () => {
+type GetPlansServerArgs = {
+  pageContext: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>;
+};
+
+export const getPlansServer = async ({ pageContext }: GetPlansServerArgs) => {
+  const protocol = pageContext?.req?.headers?.['x-forwarded-proto'];
+  const host = pageContext?.req?.headers?.host;
+  const requester = new HttpClient(`${protocol}://${host}/api`);
+
+  return requester.get<PlanDTO[]>('/offer');
+};
+
+export const getPlans = async () => {
   const requester = new HttpClient('/api');
 
   return requester.get<PlanDTO[]>('/offer');
